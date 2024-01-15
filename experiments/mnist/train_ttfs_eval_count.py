5a4d27648ac0a2fb90f29a5084bf688323843c68
Cas van der Gun <cas@vandergun.com>
2024-01-15 15:55:23 +0100

    ff

--- experiments/mnist/train_ttfs_eval_count.py
+++ experiments/mnist/train_ttfs_eval_count.py
@@ -59,8 +59,8 @@ TAU_LOSS = 0.005
 
 # Plot parameters
 EXPORT_METRICS = True
-EXPORT_DIR = Path("/content/SNN-CAPSTONE/results/train_count_eval_ttfs/output_metrics")
-SAVE_DIR = Path("/content/SNN-CAPSTONE/results/train_count_eval_ttfs/best_model")
+EXPORT_DIR = Path("/content/SNN-CAPSTONE/results/train_ttfs_eval_count/output_metrics")
+SAVE_DIR = Path("/content/SNN-CAPSTONE/results/train_ttfs_eval_count/best_model")
 
 
 def weight_initializer(n_post: int, n_pre: int) -> cp.ndarray:
