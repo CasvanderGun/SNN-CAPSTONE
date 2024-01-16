325ac9e82165234d16ce76ec1553444474fc1c38
Hannah Wener <hannah.werner@live.nl>
2024-01-16 16:21:27 +0100

    Standaard plot bestand update

--- experiments/mnist/plots/test_plot.py
+++ experiments/mnist/plots/test_plot.py
@@ -1,21 +1,60 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 
-file_path = "/Users/hanna/Downloads/SNN-CAPSTONE-1/results/train_ttfs_eval_count/output_metrics/accuracy_train_ttfs.npz"
-data = np.load(file_path)
+# file_path = "/Users/hanna/Downloads/SNN-CAPSTONE-1/results/own_loss/output_metrics/"
+# data = np.load(file_path)
 
-epochs = data["epochs"]
-values = data["values"]
+import os
 
-fig = plt.figure(figsize=(10,5))
-ax1 = fig.add_axes([0.1,0.1,0.8,0.8])
-ax1.plot(values, label="loss, TTFS")
+# Specify the path to the directory
+directory_path = '/Users/hanna/Downloads/SNN-CAPSTONE-1/results/own_loss/output_metrics (50 epoch)/'
 
-# Add labels, title, legend, etc.
+def loading_files(dic_path):
+    """ Loading .npz files from directory and storing as useable data"""
+    loaded_data = []
+    for filename in os.listdir(dic_path):
+        file_path = os.path.join(dic_path, filename)
+
+        # Check if the path is a file and has a .npz extension
+        if os.path.isfile(file_path) and filename.endswith('.npz'):
+            print(f"Loading data from file: {filename}")
+
+            data = np.load(file_path)
+            loaded_data.append({
+                "filename": filename,
+                "epochs": data['epochs'],
+                "values": data["values"]
+            })
+    return loaded_data
+
+data = loading_files(directory_path)
+
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5), gridspec_kw={'width_ratios': [1, 1, 1]})
+
+# plot the accuries together
+ax1.plot(data[0]["epochs"], data[0]["values"], color='r', label="test")
+ax1.plot(data[1]["epochs"], data[1]["values"], color='b', label="train")
 ax1.set_xlabel('Epochs')
-ax1.set_ylabel('Loss')
-ax1.set_title('Training accuracy, TTFS')
-plt.legend()
+ax1.set_ylabel("accuracy (%)")
+ax1.set_title("Accuracy")
+ax1.legend()
+
+# plot the accuries together
+ax2.plot(data[2]["epochs"], data[2]["values"], color='r', label="test")
+ax2.plot(data[3]["epochs"], data[3]["values"], color='b', label="train")
+ax2.set_xlabel('Epochs')
+ax2.set_ylabel("loss")
+ax2.set_title("Loss")
+ax2.legend()
+
+# plot the accuries together
+ax3.plot(data[4]["epochs"], data[4]["values"], color='r', label="Hidden layer")
+ax3.plot(data[5]["epochs"], data[5]["values"], color='b', label="Output layer")
+ax3.set_xlabel('Epochs')
+ax3.set_ylabel("weight norm")
+ax3.set_title("Weight norm")
+ax3.legend()
 
 # Show the plot or save it to a file
 plt.show()
+
