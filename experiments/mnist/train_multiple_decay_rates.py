6b39009599b3c401ac622b84b5f073c83c87d3c5
ChickenLord10 <96498268+ChickenLord10@users.noreply.github.com>
2024-01-17 12:37:43 +0100

    Decay rate training

---
+++ experiments/mnist/train_multiple_decay_rates.py
@@ -0,0 +1,12 @@
+from experiments.mnist import train_decay_rate
+
+decay_rates = [3, 2, 1, 0.5]
+
+paths = ["/content/SNN-CAPSTONE/results/train_decay_rate/decay_rate_3",
+         "/content/SNN-CAPSTONE/results/train_decay_rate/decay_rate_2",
+         "/content/SNN-CAPSTONE/results/train_decay_rate/decay_rate_1",
+         "/content/SNN-CAPSTONE/results/train_decay_rate/decay_rate_0.5"]
+
+for run, decay_rate in enumerate(decay_rates):
+    print(f'Run {run}\nDecay rate {decay_rate}\n')
+    train_decay_rate(paths[run], decay_rate)
