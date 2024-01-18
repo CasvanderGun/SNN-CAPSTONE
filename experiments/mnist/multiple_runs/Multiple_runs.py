f3ab521cf41c7cd32593eae039311c2eb401ae1b
Hannah Wener <hannah.werner@live.nl>
2024-01-18 15:04:01 +0100

    dd

--- experiments/mnist/multiple_runs/Multiple_runs.py
+++ experiments/mnist/multiple_runs/Multiple_runs.py
@@ -1,8 +1,8 @@
 import sys
 
-sys.path.insert(0, "../../../")  # Add repository root to python path
+sys.path.insert(0, "../")  # Add repository root to python path
 
-from experiments.mnist.multiple_runs.train_count_eval_ttfs_func import train_count_eval_ttfs_func
+from multiple_runs.train_count_eval_ttfs_func import train_count_eval_ttfs
 
 epochs = 30
 
@@ -10,4 +10,4 @@ path = '/content/SNN-CAPSTONE/results/train_multiple_runs/Run_'
 
 for run in range(5):
     print('Run: ' + str(run + 1))
-    train_count_eval_ttfs_func(epochs, path + str(run + 1))
+    train_count_eval_ttfs(epochs, path + str(run + 1))
