3fd2aba0a1709f130e1eef1f7acec466cc909cf7
ChickenLord10 <96498268+ChickenLord10@users.noreply.github.com>
2024-01-18 12:57:44 +0100

    Changed decay rate file structure

--- experiments/mnist/train_multiple_decay_rates.py
+++ experiments/mnist/train_multiple_decay_rates.py
@@ -6,11 +6,10 @@ from experiments.mnist.train_decay_rate import train_decay_rate
 
 epochs = 20
 simulation_time = 0.2
-decay_rates = [3, 2, 1, 0.5]
+decay_rates = [3, 2, 0.5]
 
 paths = ["/content/SNN-CAPSTONE/results/train_decay_rate/decay_rate_3",
          "/content/SNN-CAPSTONE/results/train_decay_rate/decay_rate_2",
-         "/content/SNN-CAPSTONE/results/train_decay_rate/decay_rate_1",
          "/content/SNN-CAPSTONE/results/train_decay_rate/decay_rate_0.5"]
 
 for run, decay_rate in enumerate(decay_rates):
