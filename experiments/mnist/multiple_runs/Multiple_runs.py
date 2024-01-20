36868a22a3c801d641ec5c887c4caa4c947756b0
Hannah Wener <hannah.werner@live.nl>
2024-01-20 22:52:34 +0100

    multiple runs with neuron plot

--- experiments/mnist/multiple_runs/Multiple_runs.py
+++ experiments/mnist/multiple_runs/Multiple_runs.py
@@ -1,13 +1,15 @@
 import sys
+import os
 
 sys.path.insert(0, "../")  # Add repository root to python path
 
 from multiple_runs.train_count_eval_ttfs_func import train_count_eval_ttfs
 
-epochs = 30
+epochs = 2
 
-path = '/kaggle/working/SNN-CAPSTONE/results/train_multiple_runs/Run_'
+path = '/content/SNN-CAPSTONE/results/train_multiple_runsv2/Run_'
 
-for run in range(5):
+for run in range(2):
     print('Run: ' + str(run + 1))
+    os.makedirs(path + str(run + 1), exist_ok=True)
     train_count_eval_ttfs(epochs, path + str(run + 1))
