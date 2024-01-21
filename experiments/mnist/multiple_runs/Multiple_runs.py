d6efb67c71a8ee00ee857177138b46dc8be3ba7a
Hannah Wener <hannah.werner@live.nl>
2024-01-21 11:20:00 +0100

    full run

--- experiments/mnist/multiple_runs/Multiple_runs.py
+++ experiments/mnist/multiple_runs/Multiple_runs.py
@@ -5,8 +5,8 @@ sys.path.insert(0, "../")  # Add repository root to python path
 
 from multiple_runs.train_count_eval_ttfs_func import train_count_eval_ttfs
 
-epochs = 2
-num_runs = 2
+epochs = 30
+num_runs = 5
 
 path = '/kaggle/working/SNN-CAPSTONE/results/train_multiple_runsv2/Run_'
 
