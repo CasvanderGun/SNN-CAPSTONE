e36160aa1d7a7033b7be8b098dbbbc9923d20a08
Hannah Wener <hannah.werner@live.nl>
2024-01-22 14:45:35 +0100

    Change to training

--- experiments/mnist/multiple_runs/multiple_runs_ttfs_eval_count.py
+++ experiments/mnist/multiple_runs/multiple_runs_ttfs_eval_count.py
@@ -6,9 +6,9 @@ sys.path.insert(0, "../")  # Add repository root to python path
 from multiple_runs.train_ttfs_eval_count_func import train_ttfs_eval_count
 
 epochs = 30
-num_runs = 5
+num_runs = 2
 
-path = '/kaggle/working/SNN-CAPSTONE/results/train_multiple_runsv3/Run_'
+path = '/kaggle/working/SNN-CAPSTONE/results/train_ttfs_eval_count/train_multiple_runs/Run_'
 
 for run in range(num_runs):
     print('Run: ' + str(run + 1))
