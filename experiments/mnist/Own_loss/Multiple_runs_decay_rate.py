8520fa437dfd4f87aba44696df5393441da45a06
ChickenLord10 <96498268+ChickenLord10@users.noreply.github.com>
2024-01-23 10:50:41 +0100

    ff

--- experiments/mnist/Own_loss/Multiple_runs_decay_rate.py
+++ experiments/mnist/Own_loss/Multiple_runs_decay_rate.py
@@ -5,10 +5,10 @@ sys.path.insert(0, "../../../")  # Add repository root to python path
 
 from experiments.mnist.Own_loss.train_decay_rate import train_decay_rate
 
-epochs = 1
+epochs = 30
 simulation_time = 0.2
 decay_rate = 1
-runs = 3
+runs = 5
 
 root_path = '/kaggle/working/SNN-CAPSTONE/'
 path = root_path + "results/train_decay_rate/multiple_runs/Run_"
