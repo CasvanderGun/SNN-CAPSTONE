890726e94d9f4a697dc17ca22e56da0046d238e3
Cas van der Gun <47693005+CasvanderGun@users.noreply.github.com>
2024-01-24 10:20:55 +0100

    Update train_ttfs_eval_ttfs_single_spike.py

--- experiments/mnist/Standard_eval/train_ttfs_eval_ttfs_single_spike.py
+++ experiments/mnist/Standard_eval/train_ttfs_eval_ttfs_single_spike.py
@@ -254,7 +254,7 @@ if __name__ == "__main__":
         avg_spike_counts = {digit: np.mean(spike_counts[digit], axis=0) for digit in spike_counts}
 
         # Create a figure to visualize network activity and sparsity
-        create_spike_count_map(avg_output_spike_counts, 10, 1, f'OUTPUTSpikeCountMap_10Neurons_TTFS_TTFS_SINGLE_Epoch{epoch + 1}', 'train_ttfs_eval_ttfs_single_spike', '/content/SNN-CAPSTONE/')
+        create_spike_count_map(avg_output_spike_counts, 10, 1, f'OUTPUTSpikeCountMap_10Neurons_TTFS_TTFS_SINGLE_Epoch{epoch + 1}', 'train_ttfs_eval_ttfs_single_spike', '/kaggle/working/SNN-CAPSTONE/')
 
-        create_spike_count_map(avg_spike_counts, 800, 1, f'SpikeCountMap_800Neurons_TTFS_TTFS_SINGLE_Epoch{epoch + 1}', 'train_ttfs_eval_ttfs_single_spike', '/content/SNN-CAPSTONE/')
-        create_spike_count_map(avg_spike_counts, 100, 1, f'SpikeCountMap_100Neurons_TTFS_TTFS_SINGLE_Epoch{epoch + 1}', 'train_ttfs_eval_ttfs_single_spike', '/content/SNN-CAPSTONE/')
+        create_spike_count_map(avg_spike_counts, 800, 1, f'SpikeCountMap_800Neurons_TTFS_TTFS_SINGLE_Epoch{epoch + 1}', 'train_ttfs_eval_ttfs_single_spike', '/kaggle/working/SNN-CAPSTONE/')
+        create_spike_count_map(avg_spike_counts, 100, 1, f'SpikeCountMap_100Neurons_TTFS_TTFS_SINGLE_Epoch{epoch + 1}', 'train_ttfs_eval_ttfs_single_spike', '/kaggle/working/SNN-CAPSTONE/')
