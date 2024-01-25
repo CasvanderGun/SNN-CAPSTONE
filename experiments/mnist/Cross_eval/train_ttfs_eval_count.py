1e55cbb4f1557931bd47ade8fd1f220ba8a597b0
Tim Albers <alberstim12@gmail.com>
2024-01-25 10:46:31 +0100

    added output_spike_count_map with non-pee colors

--- experiments/mnist/Cross_eval/train_ttfs_eval_count.py
+++ experiments/mnist/Cross_eval/train_ttfs_eval_count.py
@@ -7,7 +7,7 @@ import sys
 sys.path.insert(0, "../../../")  # Add repository root to python path
 
 from experiments.mnist.load_model import plot_spike_train, get_image
-from experiments.mnist.plots.neuron_plot import create_spike_count_map
+from experiments.mnist.plots.neuron_plot import create_spike_count_map, create_output_spike_count_map
 from experiments.mnist.Dataset import Dataset
 from bats.Monitors import *
 from bats.Layers import InputLayer, LIFLayer
@@ -275,7 +275,7 @@ if __name__ == "__main__":
         avg_output_spike_counts = {digit: np.mean(output_spike_counts[digit], axis=0) for digit in output_spike_counts}
 
         # Create a figure to visualize network activity and sparsity
-        create_spike_count_map(avg_output_spike_counts, 10, 15, f'OUTPUTSpikeCountMap_10Neurons_TTFS_Count_Epoch{epoch + 1}', '/results/train_ttfs_eval_count', '/kaggle/working/SNN-CAPSTONE')
+        create_output_spike_count_map(avg_output_spike_counts, 10, 15, f'OUTPUTSpikeCountMap_10Neurons_TTFS_Count_Epoch{epoch + 1}', '/results/train_ttfs_eval_count', '/kaggle/working/SNN-CAPSTONE')
         
         create_spike_count_map(avg_spike_counts, 800, 15, f'SpikeCountMap_800Neurons_TTFS_Count_Epoch{epoch + 1}', '/results/train_ttfs_eval_count', '/kaggle/working/SNN-CAPSTONE')
         create_spike_count_map(avg_spike_counts, 100, 15, f'SpikeCountMap_100Neurons_TTFS_Count_Epoch{epoch + 1}', '/results/train_ttfs_eval_count', '/kaggle/working/SNN-CAPSTONE')
